cb0c94dc32cb21fb5d8cc7c8cad0b2a855119cd6
techakki <akshaykmr869@gmail.com>
2018-12-26 12:41:42 +0530

    Commit ReactJs Web application

---
+++ src/Components/List.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+                <h3>List of Places</h3>
+            </div>
+        );
+    }
+};
